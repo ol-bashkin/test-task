c3f292b829e66867536df70dd06f601898d8f4e2
Олег Башкин <ol.bashkin@gmail.com>
2020-08-10 00:09:03 +0300

    router

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,52 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import store from "@/store/";
 
 Vue.use(VueRouter);
 
 const routes = [
     {
         path: "/",
-        name: "Home",
-        component: Home
+        name: "home",
+        component: Home,
+        beforeEnter(routeTo, routeFrom, next) {
+            store.state.user.user !== null ? next("/welcome") : next();
+        }
     },
     {
-        path: "/register",
-        name: "Registration",
-        component: () => import("../views/Registration.vue")
-    }
+        path: "/registration",
+        name: "registration",
+        component: () => import("../views/Registration.vue"),
+        beforeEnter(routeTo, routeFrom, next) {
+            store.state.user.user !== null ? next("/welcome") : next();
+        }
+    },
+    {
+        path: "/welcome",
+        name: "welcome",
+        component: () => import("../views/Welcome.vue"),
+        props: true,
+        meta: { requireAuth: true }
+    },
+    { path: "*", redirect: "/" }
 ];
 
 const router = new VueRouter({
     mode: "history",
     base: process.env.BASE_URL,
     routes
+});
+
+router.beforeEach((routeTo, routeFrom, next) => {
+    const userString = localStorage.getItem("user");
+    if (!store.state.user.user && userString) {
+        const userData = JSON.parse(userString);
+        store.commit("user/SET_USER_DATA", userData);
+    }
+    routeTo.matched.some(record => record.meta.requireAuth) && !userString
+        ? next("/")
+        : next();
 });
 
 export default router;
